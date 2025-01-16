94a5afd90977b8d6a163563081a4f333e619633c
gili-julio <giliardoplay@gmail.com>
2025-01-16 00:40:24 -0300

    initial spring config

---
+++ src/main/java/br/ufrn/imd/SIGResAPI/SigResApiApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.SIGResAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigResApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigResApiApplication.class, args);
+	}
+
+}
